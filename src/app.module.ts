65cdcc91ff7d6a43e557994dfae0adbabc645e00
rahul7840 <coading2020@gmail.com>
2023-04-13 18:24:40 +0530

    Inital Commit

---
+++ src/app.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { AuthModule } from './auth/auth.module';
+import { BookmarkModule } from './bookmark/bookmark.module';
+import { UserModule } from './user/user.module';
+import { PrismaModule } from './prisma/prisma.module';
+
+@Module({
+  imports: [AuthModule, BookmarkModule, UserModule, PrismaModule],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
